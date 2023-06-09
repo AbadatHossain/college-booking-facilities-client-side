67675d448344b9523550cc8ea38dc4455fb2a473
abadat <abadat26@gmail.com>
2023-06-09 20:49:11 +0600

    required studet components ready in my projects

---
+++ src/components/RequiredStudent.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from "../providers/AuthProvider";
+import { useContext } from "react";
+import axios from 'axios';
+
+
+const RequiredStudent = ({ children }) => {
+  const [student, setStudent] = useState();
+  const [loading, setLoading] = useState(false);
+  const user = useContext(AuthContext);
+  console.log(student, user);
+
+  useEffect(() => {
+    if (user.user) {
+      setLoading(true);
+      axios.get(`http://localhost:8000/user/${user.user.email}`)
+        .then(res => {
+          if (res.data) {
+            setLoading(false);
+            setStudent(true);
+          } else {
+            setLoading(false);
+            setStudent(false);
+          }
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+    }
+  }, [user.user])
+
+  
+
+};
+
+export default RequiredStudent;
