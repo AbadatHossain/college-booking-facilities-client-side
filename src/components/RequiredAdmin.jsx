d0c799f99b2aad36e3ea248b268fe4a8abb27b74
abadat <abadat26@gmail.com>
2023-06-10 21:46:17 +0600

    require admin is completed

---
+++ src/components/RequiredAdmin.jsx
@@ -0,0 +1,50 @@
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
+import axios from 'axios';
+// import { Navigate } from 'react-router-dom';
+
+const RequiredAdmin = ({ children }) => {
+  const [admin, setAdmin] = useState("");
+  const [loading, setLoading] = useState(false);
+  const user = useContext(AuthContext);
+  console.log(admin);
+
+  useEffect(() => {
+    if (user.user) {
+      setLoading(true);
+      axios.get(`http://localhost:8000/checkAdmin/${user.user.email}`)
+        .then(res => {
+          console.log(res.data);
+          if (res.data) {
+            setLoading(false);
+            setAdmin(true);
+          } else {
+            setLoading(false);
+            setAdmin(false);
+          }
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+    }
+  }, [user.user])
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  } else if (!loading && admin) {
+    return children;
+  }
+
+  // if (!user.user) {
+  //   return <Navigate to="/login" replace />
+  // } else if (admin) {
+  //   return children;
+  // }
+};
+
+export default RequiredAdmin;
